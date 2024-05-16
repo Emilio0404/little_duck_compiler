eb8c40d775ed8c9b4199e31d04ca9926e0f655d6
Emilio0404 <emilio.alvear@hotmail.com>
2024-05-16 15:17:16 -0600

    Added semantic cube

---
+++ CuboSemantico.py
@@ -0,0 +1,44 @@
+semantic_cube = {
+    'int': {
+            'int': {
+                    '+': 'int', 
+                    '-': 'int', 
+                    '*': 'int', 
+                    '/': 'int', 
+                    '=': 'int', 
+                    '!=': 'int', 
+                    '>': 'int', 
+                    '<': 'int'
+                },
+            'float': {
+                    '+': 'float', 
+                    '-': 'float', 
+                    '*': 'float', 
+                    '/': 'float', 
+                    '=': 'float', 
+                    '!=': 'float', 
+                    '>': 'float', 
+                }
+        },
+    'float': {
+            'int': {
+                    '+': 'float', 
+                    '-': 'float', 
+                    '*': 'float', 
+                    '/': 'float', 
+                    '=': 'float', 
+                    '!=': 'float', 
+                    '>': 'float', 
+                    '<': 'float'
+                },
+            'float': {
+                    '+': 'float', 
+                    '-': 'float', 
+                    '*': 'float', 
+                    '/': 'float', 
+                    '=': 'float', 
+                    '!=': 'float', 
+                    '>': 'float', 
+                }
+        }
+}
